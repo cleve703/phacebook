57cb11957f4b5e5a85d5fdd3ee93f82f88794946
jay <cleve703@gmail.com>
2020-06-19 09:43:05 -0400

    add friendship model and friend request method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+
+  def friend_request(user)
+    Friendship.create!(user: self, friend: user, confirmed: false)
+  end
 end
