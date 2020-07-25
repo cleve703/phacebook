98a65d18f816832acc85b6c8f501b05bca0b3997
jay <cleve703@gmail.com>
2020-07-25 08:51:59 -0400

    add confirm friend request and related methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,16 @@ class User < ApplicationRecord
   has_many :invitations_received, class_name: 'Friendship', foreign_key: 'invitee_id'
   has_many :invitees, through: :invitations_sent
   has_many :inviters, through: :invitations_received
+  
+  def make_friend_request(target, origin=self)
+    Friendship.create(inviter_id: origin.id, invitee_id: target.id)
+  end
 
   def accepted_invitations
     self.invitations_received.where(confirmed:true).each { |inv| inv.invitee_id } + 
     self.invitations_sent.where(confirmed:true).each { |inv| inv.inviter_id }
   end
-
+  
   def friends
     friend_list = []
     accepted_invitations.each do |inv|
@@ -24,5 +28,17 @@ class User < ApplicationRecord
     end
     User.where(id: friend_list)
   end
+
+  def outstanding_friend_requests
+    self.invitations_received.where(confirmed:false)
+  end
+
+  def confirm_friend_request(id)
+    friendship = Friendship.find_by(id:id)
+    if friendship.invitee == self
+      friendship.update(confirmed: true)
+    end
+  end
+
 
 end
