df4d26a9f509eeb69589a168fd483433e86b71e2
Jay <cleve703@gmail.com>
2020-08-23 00:33:48 -0400

    modify user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,26 @@
 class UserMailer < ApplicationMailer
-  default from: 'phacebook@codingcop.com'
+  require 'sendgrid-ruby'
+  include SendGrid
  
   def welcome_email(user)
     @user = user
     @url  = 'codingcop.com'
-    mail(to: @user.email, subject: 'Welcome to Phacebook')
+    from = SendGrid::Email.new(email: 'no-reply@codingcop.com')
+    to = SendGrid::Email.new(email: @user.email)
+    subject = 'Welcome to Phacebook'
+    content = SendGrid::Content.new(type: 'text/plain', value: 'The best new web app around.')
+    mail = SendGrid::Mail.new(from, subject, to, content)
+  
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    puts response.status_code
+    puts response.body
+    puts response.parsed_body
+    puts response.headers
+  
   end
+
+
+
+
 end
